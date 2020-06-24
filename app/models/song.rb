40fa7634d7254abe41913bf4eb8169601d2269f3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-24 19:48:24 +0000

    Automatically backed up by Learn

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
+  
+  validates :released, inclusion: { in: [true, false] }
+end

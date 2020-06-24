f4edc403658e04c0d7f3d786f59dc5b93e92aec8
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-24 20:39:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-RRails.application.routes.draw do
+Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/new', to: 'songs#new', as: 'new_song'
   post '/songs', to: 'songs#create'

b65d8ae0e75ec76692ed7fd9760eb83211ea37b7
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-24 20:26:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,12 @@ class SongsController < ApplicationController
   def index 
     @songs = Song.all 
   end 
+  
+  def show 
+  end 
+  
+  private 
+  def find_song 
+    @song = Song.find()
+  
 end
